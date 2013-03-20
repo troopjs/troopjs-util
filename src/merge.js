60949ef202ee51b23cce6a82ef2170acac0619d6
Mikael Karon <mikael@karon.se>
2013-03-20 14:28:35 +0800

    Safer merge with tests

--- src/merge.js
+++ src/merge.js
@@ -3,38 +3,65 @@
  * @license MIT http://troopjs.mit-license.org/ © Mikael Karon mailto:mikael@karon.se
  */
 /*global define:false */
-define(function MergeModule() {
+define([ "poly/object" ], function MergeModule() {
 	/*jshint strict:false */
 
-	var ARRAY = Array;
-	var OBJECT = Object;
+	var LENGTH = "length";
+	var ARRAY_PROTO = Array.prototype;
+	var ARRAY_CONCAT = ARRAY_PROTO.concat;
+	var OBJECT_PROTO = Object.prototype;
+	var OBJECT_TOSTRING = OBJECT_PROTO.toString;
+	var TOSTRING_OBJECT = OBJECT_TOSTRING.call(OBJECT_PROTO);
+	var TOSTRING_ARRAY = OBJECT_TOSTRING.call(ARRAY_PROTO);
 
 	return function merge(source) {
 		var target = this;
-		var key = null;
+		var key;
+		var keys;
 		var i;
+		var j;
 		var iMax;
-		var value;
-		var constructor;
+		var jMax;
+		var source_value;
+		var target_value;
+		var source_tostring;
+		var target_tostring;
 
-		for (i = 0, iMax = arguments.length; i < iMax; i++) {
+		// Iterate arguments
+		for (i = 0, iMax = arguments[LENGTH]; i < iMax; i++) {
+			// Get source
 			source = arguments[i];
 
-			for (key in source) {
-				value = source[key];
-				constructor = value.constructor;
-	
+			// Get source keys
+			keys = Object.keys(source);
+
+			// Iterate keys
+			for (j = 0, jMax = keys[LENGTH]; j < jMax; j++) {
+				key = keys[j];
+				source_value = source[key];
+				target_value = target[key];
+
+				// No merge - copy source_value
 				if (!(key in target)) {
-					target[key] = value;
+					target[key] = source_value;
+					continue;
 				}
-				else if (constructor === ARRAY) {
-					target[key] = target[key].concat(value);
+
+				// Get 'types'
+				source_tostring = OBJECT_TOSTRING.call(source_value);
+				target_tostring = OBJECT_TOSTRING.call(target_value);
+
+				// Can we merge objects?
+				if (target_tostring === TOSTRING_OBJECT && source_tostring === TOSTRING_OBJECT) {
+					merge.call(target_value, source_value);
 				}
-				else if (constructor === OBJECT) {
-					merge.call(target[key], value);
+				// Can we merge arrays?
+				else if (target_tostring === TOSTRING_ARRAY && source_tostring === TOSTRING_ARRAY) {
+					target[key] = ARRAY_CONCAT.call(target_value, source_value);
 				}
+				// No merge - override target[key]
 				else {
-					target[key] = value;
+					target[key] = source_value;
 				}
 			}
 		}
