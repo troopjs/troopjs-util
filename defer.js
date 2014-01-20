1bc9dc49717da37460e51ffa133926c937a0daf2
Mikael Karon <mikael@karon.se>
2014-01-20 15:22:08 +0800

    Rewrite together with Garry

--- defer.js
+++ defer.js
@@ -1,38 +1,38 @@
 /**
+ * TroopJS utils/defer
  * when.defer patched with jQuery/deferred compatibility.
  * @license MIT http://troopjs.mit-license.org/ © Mikael Karon mailto:mikael@karon.se
  */
-define(["when"], function DeferedModule(when) {
+define([ "when", "poly/array" ], function DeferModule(when) {
 	"use strict";
 
 	var ARRAY_SLICE = Array.prototype.slice;
 
-	// Since the deferred implementation in jQuery (that we use in 1.x) allows
-	// to resolve with optional context and multiple arguments, we monkey-patch resolve here
-	function patch(deferred, method) {
-		var func = deferred.resolver[method];
-		// Resolve/Reject/Progress with parameters:
-		// http://api.jquery.com/deferred.resolve
-		deferred[method] = function () {
-			func(ARRAY_SLICE.call(arguments));
-		};
-
-		// Resolve/Reject/Progress with context and parameters:
-		// http://api.jquery.com/deferred.resolveWith
-		deferred[method + 'With'] = function (context) {
-			func.apply(context, ARRAY_SLICE.call(arguments, 1));
-		};
-	}
-
 	return function Defer() {
+		// Create defer
+		var defer = when.defer();
 
-		// Create deferred
-		var deferred = when.defer();
+		[ "resolve", "reject", "progress" ].forEach(function (value) {
+			var me = this;
 
-		patch(deferred, 'resolve');
-		patch(deferred, 'reject');
-		patch(deferred, 'progress');
+			// Since the deferred implementation in jQuery (that we use in 1.x) allows
+			// to resolve with optional context and multiple arguments, we monkey-patch resolve here
+			var func = me.resolver[value];
 
-		return deferred;
+			// Resolve/Reject/Progress with parameters:
+			// http://api.jquery.com/deferred.resolve
+			me[value] = function () {
+				func.apply(me, arguments);
+			};
+
+			// Resolve/Reject/Progress with context and parameters:
+			// http://api.jquery.com/deferred.resolveWith
+			me[value + "With"] = function (context) {
+				func.apply(context, ARRAY_SLICE.call(arguments, 1));
+			};
+
+		}, defer);
+
+		return defer;
 	};
 });
