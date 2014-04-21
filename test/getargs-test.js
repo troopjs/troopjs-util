b3d6e7575defee155a855f607ea6b2a2b815ea10
Mikael Karon <mikael@karon.se>
2014-04-21 18:44:59 +0800

    Implement named args.

--- test/getargs-test.js
+++ test/getargs-test.js
@@ -28,6 +28,28 @@ buster.testCase("troopjs-util/getargs", function (run) {
 
 			" '1, 2 ',  3,\"4\", 5 " : function () {
 				assert.equals(getargs.call(" '1, 2 ',  3,\"4\", 5 "), [ "1, 2 ", 3, "4", 5 ]);
+			},
+
+			"1,key=2,3": function () {
+				var expected = [1, 2, 3];
+				expected["key"] = expected[1];
+
+				var actual = getargs.call("1,key=2,3");
+
+				assert.match(actual, expected);
+				assert.same(actual[1], actual["key"]);
+			},
+
+			"1,'quoted=key,value'='test',key=false": function () {
+				var expected = [1, "test", false];
+				expected["quoted=key,value"] = expected[1];
+				expected["key"] = expected[2];
+
+				var actual = getargs.call("1,'quoted=key,value'='test',key=false");
+
+				assert.match(actual, expected);
+				assert.same(actual[1], actual["quoted=key,value"]);
+				assert.same(actual[2], actual["key"]);
 			}
 		});
 	});
